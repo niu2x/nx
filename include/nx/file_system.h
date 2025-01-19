ef8a16f0521b46624bb9cc104bc2fa0c77c693d1
niu2x <niu2x@icloud.com>
2025-01-20 02:26:45 +0800

    fix

--- include/nx/file_system.h
+++ include/nx/file_system.h
@@ -4,4 +4,9 @@
 
 namespace nx::file_system {
 
+bool is_directory(const String& path);
+
+bool is_file(const String& path);
+
+bool exists(const String& path);
 }
--- src/file_system.cpp
+++ src/file_system.cpp
@@ -1,1 +1,18 @@
 #include <nx/file_system.h>
+
+#include <filesystem>
+
+namespace std_fs = std::filesystem;
+
+namespace nx::file_system {
+
+bool is_directory(const String& path) { return std_fs::is_directory(path); }
+
+bool is_file(const String& path)
+{
+    return exists(path) && !(is_directory(path));
+}
+
+bool exists(const String& path) { return std_fs::exists(path); }
+
+} // namespace nx::file_system
