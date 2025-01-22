c72e1dd78e1157b5e86130c185d6f8c047474dfe
xiaorong.niu <xiaorong.niu@happyelements.com>
2025-01-22 12:43:56 +0800

    fix

--- include/nx/type.h
+++ include/nx/type.h
@@ -204,5 +204,5 @@ private:
 #define NX_CLEAR_BIT(x, bit)    ((x) & (~(1 << (bit))))
 
 #define NX_GET_BIT_BOOL(x, bit) (NX_GET_BIT((x), (bit)) == 1)
-#define NX_SET_BIT_BOOL(x, bit)                                                \
-    ((bit) ? NX_SET_BIT((x), (bit)) : NX_CLEAR_BIT((x), (bit)))
+#define NX_SET_BIT_BOOL(x, bit, value)                                         \
+    ((value) ? NX_SET_BIT((x), (bit)) : NX_CLEAR_BIT((x), (bit)))
