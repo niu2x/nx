02d9ac781b4fe2fe829ab8171e183ab024942859
xiaorong.niu <xiaorong.niu@happyelements.com>
2025-03-18 18:13:13 +0800

    fix

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,8 @@
 #include <gtest/gtest.h>
+#include <nx/alias.h>
 
 TEST(SampleTest, AssertionTrue) { EXPECT_TRUE(true); }
+TEST(file_system, test) {  
+	auto result = nx::fs::read_file("/no_such_file");
+	EXPECT_EQ(std::get<nx::IO_Error>(result), nx::IO_Error::NOT_OPEN);
+}
